1d8e17ff913bd957eca95d1ef39fedf625c90c22
WilliamEduc <kwa2253974@slu.edu.ph>
2025-06-18 12:50:04 +0800

    Add files via upload

---
+++ CodeBase/CodeBase/Main.java
@@ -0,0 +1,30 @@
+package CodeBase;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to ABC Payroll System");
+        /**
+         *
+         * Complete the code
+         */
+    }
+
+    public static void displayPayroll() {
+        Scanner scanner = new Scanner(System.in);
+        /**
+         *
+         * Complete the code
+         */
+    }
+
+    public static void printPayslip() {
+
+
+        System.out.println("\nABC Solutions - Employee Payslip (2025)");
+        System.out.println("=================================");
+
+        System.out.println("=================================");
+    }
+}
---
+++ CodeBase/CodeBase/SalaryCalculations.java
@@ -0,0 +1,23 @@
+package CodeBase;
+
+public class SalaryCalculations {
+    // Method to calculate overtime pay
+    public double calculateOvertimePay() {
+        return ;
+    }
+   
+    //Method to calculate gross pay
+    public double calculateGrossPay() {
+        return ;
+    }
+
+    //Method to calculate total deductions
+    public double totalDeductions() {
+        return ;
+    }
+
+    //Method to calculate total net pay
+    public double calculateNetPay() {
+        return ;
+    }
+}
