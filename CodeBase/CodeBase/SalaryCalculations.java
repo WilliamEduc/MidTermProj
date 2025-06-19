d0880bba4f29975ff2c6986839a67533c383e8a0
gillianne27 <gpg2254139@slu.edu.ph>
2025-06-19 12:29:45 +0800

    Update SalaryCalculations.java

--- CodeBase/CodeBase/SalaryCalculations.java
+++ CodeBase/CodeBase/SalaryCalculations.java
@@ -1,23 +1,65 @@
-//package CodeBase;
-//
-//public class SalaryCalculations {
-//    // Method to calculate overtime pay
-//    public double calculateOvertimePay() {
-//        return ;
-//    }
-//
-//    //Method to calculate gross pay
-//    public double calculateGrossPay() {
-//        return ;
-//    }
-//
-//    //Method to calculate total deductions
-//    public double totalDeductions() {
-//        return ;
-//    }
-//
-//    //Method to calculate total net pay
-//    public double calculateNetPay() {
-//        return ;
-//    }
-//}
+package CodeBase;
+
+public class SalaryCalculations {
+    private String name;
+    private double monthlySalary;
+    private double hoursWorked;
+
+      public Employee(String name, double monthlySalary, double hoursWorked){
+        this.name = name;
+        this.monthlySalary= monthlySalary;
+        this.hoursWorked = hoursWorked;
+    }
+      public void setName(String name) {
+        this.name = name;
+    }
+  
+    public void setMonthlySalary(double monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+    public String getName(){
+        return name;
+    }
+  
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getHourlyRate(){
+        return monthlySalary / 160;
+    }
+  
+   // Method to calculate overtime pay
+   public double calculateOvertimePay() {
+             if (hoursWorked > 160){
+            overtimeHours = hoursWorked - 160;
+        }
+        return overtimeHours * getHourlyRate() * 1.25;
+    }
+  
+      public void display(){
+        System.out.println("Full name: " + name);
+        System.out.println("Monthly Salary: " + monthlySalary);
+        System.out.println("Overtime Hours Worked: " + hoursWorked);
+        System.out.println("Overtime Pay: " + calculateOvertimePay());
+    }
+  
+   //Method to calculate gross pay
+   public double calculateGrossPay() {
+       return ;
+   }
+
+   //Method to calculate total deductions
+   public double totalDeductions() {
+       return ;
+   }
+
+   //Method to calculate total net pay
+   public double calculateNetPay() {
+       return ;
+   }
+}
