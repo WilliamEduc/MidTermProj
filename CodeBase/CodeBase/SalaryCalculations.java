014331a0575b6f12005de376285923c9264e2068
gillianne27 <gpg2254139@slu.edu.ph>
2025-06-20 13:13:59 +0800

    Update SalaryCalculations.java

--- CodeBase/CodeBase/SalaryCalculations.java
+++ CodeBase/CodeBase/SalaryCalculations.java
@@ -1,53 +1,54 @@
 package CodeBase;
 
 public class SalaryCalculations {
-    private String name;
-    private double monthlySalary;
-    private double hoursWorked;
+        private String name;
+    private double salary;
+    private double otHours;
 
-      public Employee(String name, double monthlySalary, double hoursWorked){
-        this.name = name;
-        this.monthlySalary= monthlySalary;
-        this.hoursWorked = hoursWorked;
-    }
-      public void setName(String name) {
-        this.name = name;
-    }
-  
-    public void setMonthlySalary(double monthlySalary) {
-        this.monthlySalary = monthlySalary;
-    }
-
-    public void setHoursWorked(double hoursWorked) {
-        this.hoursWorked = hoursWorked;
+    public Overtime(){
+        name = " ";
+        salary = 0.00;
+        otHours = 0.00;
     }
     public String getName(){
         return name;
     }
-  
-    public double getHoursWorked() {
-        return hoursWorked;
+
+    public double getSalary() {
+        return salary;
     }
 
+    public double getOtHours() {
+        return otHours;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setOtHours(double otHours) {
+        this.otHours = otHours;
+    }
     public double getHourlyRate(){
-        return monthlySalary / 160;
+        double hourlyRate = salary / 160.0;
+        return hourlyRate;
     }
-  
-   // Method to calculate overtime pay
-   public double calculateOvertimePay() {
-             if (hoursWorked > 160){
-            overtimeHours = hoursWorked - 160;
-        }
-        return overtimeHours * getHourlyRate() * 1.25;
+    public double calculateOvertimePay(){
+        double otRate = getHourlyRate() * 1.25;
+        return otRate * getOtHours();
     }
-  
-      public void display(){
-        System.out.println("Full name: " + name);
-        System.out.println("Monthly Salary: " + monthlySalary);
-        System.out.println("Overtime Hours Worked: " + hoursWorked);
+    public void display(){
+
+        System.out.println("Full name: " + getName());
+        System.out.println("Monthly Salary: " + getSalary());
+        System.out.println("Overtime Hours Worked: " + getOtHours());
         System.out.println("Overtime Pay: " + calculateOvertimePay());
     }
-  
+
    //Method to calculate gross pay
    public double calculateGrossPay() {
        return ;
