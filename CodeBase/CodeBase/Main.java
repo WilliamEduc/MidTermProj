64dec3f126075986df4854660d3fce5b0bc3786c
WilliamEduc <kwa2253974@slu.edu.ph>
2025-06-23 13:06:46 +0800

    Update Main.java

--- CodeBase/CodeBase/Main.java
+++ CodeBase/CodeBase/Main.java
@@ -1,13 +1,10 @@
-package CodeBase;
-
+package Project.CodeBase.CodeBase;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Payroll employee = new Payroll();                                               //tols, gumagana mga edits ko sa GIT ko
-        SalaryCalculations calculations = new SalaryCalculations();
 
+        Payroll employee = new Payroll();                                               //tols, gumagana mga edits ko sa GIT ko
         employee.InputEmployeeDetails();
     }
 }
